d16da96609d54b62b8cf91436ba2c28f9ac2f61f
Manuperex <104181677+Manuperex@users.noreply.github.com>
2022-12-16 15:30:17 -0500

    Primer commit, creado sidebar

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+function App() {
+  return (
+    <div class="container-fluid">
+    <div class="row flex-nowrap">
+        <div class="col-auto col-fluid-3 col-fluid-2 px-sm-2 px-0 bg-dark">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                {/* <a href="/" class="d-flex align-items-center pb-5 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <span class="fs-5 d-none d-sm-inline">M.P.</span>
+                </a> */}
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start pt-5" id="menu">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link align-middle px-0">
+                            <img class="fs-4" src="../public/Portafolio/bootstrap.png" width="50"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <img class="fs-4" src="../public/Portafolio/bootstrap.png" width="50"/> </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-0 align-middle">
+                        <img class="fs-4" src="../public/Portafolio/bootstrap.png" width="50"/> </a>
+                    </li>
+                    <li>
+                        <a href="#" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
+                        <img class="fs-4" src="../public/Portafolio/bootstrap.png" width="50"/> </a>
+                    </li>
+                    <li>
+                        <a href="#" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <img class="fs-4" src="../public/Portafolio/bootstrap.png" width="50"/> </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-0 align-middle">
+                        <img class="fs-4" src="../public/Portafolio/bootstrap.png" width="50"/>  </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col py-3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem sit eveniet accusamus error optio quibusdam ducimus id ab aut commodi dolorem ut, repellat natus labore molestiae? Eveniet est in quae.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ullam commodi fuga non magnam aut reprehenderit alias laboriosam eos, quasi ut animi quia quam distinctio accusamus veritatis, similique exercitationem perspiciatis?
+            Mollitia eos autem nisi nulla ad voluptate laudantium sed, sint totam eaque minus et sequi, dicta nostrum maiores enim accusamus commodi inventore nam dolorem aspernatur magni veniam. At, unde odit.
+        </div>
+    </div>
+</div>
+    
+  );
+}
+
+export default App;
